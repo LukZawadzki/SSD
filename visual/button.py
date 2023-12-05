08afcb0f134c7a884ca84b5d04bd25e19b99b796
LukZawadzki <lzawadzki@student.agh.edu.pl>
2023-12-05 19:16:20 +0100

    Add pause button

--- visual/button.py
+++ visual/button.py
@@ -3,9 +3,10 @@ import pygame as pg
 
 class Button:
     def __init__(self, screen, x, y, width, height, text, text_size, text_color, button_color):
+        self.text_color = text_color
         self.Rect = pg.Rect(x, y, width, height)
         self.Font = pg.font.Font(None, text_size)
-        self.Text = self.Font.render(text, True, text_color)
+        self.Text = self.Font.render(text, True, self.text_color)
         self.screen = screen
         self.button_color = button_color
 
@@ -13,3 +14,6 @@ class Button:
         pg.draw.rect(self.screen, self.button_color, self.Rect, 2)
         self.screen.blit(self.Text, (self.Rect.centerx - self.Text.get_width() // 2,
                                      self.Rect.centery - self.Text.get_height() // 2))
+
+    def change_text(self, new_text):
+        self.Text = self.Font.render(new_text, True, self.text_color)
--- visual/display.py
+++ visual/display.py
@@ -1,6 +1,7 @@
 from visual.button import Button
 from visual.utils import *
 
+
 def run():
     pg.init()
 
@@ -8,10 +9,13 @@ def run():
 
     reset_button = Button(screen, 15, pg.display.get_surface().get_size()[1] - 50, 75, 40,
                           "Reset", 24, (0, 0, 0), (0, 0, 0))
+    pause_button = Button(screen, 15+75+15, pg.display.get_surface().get_size()[1] - 50, 95, 40,
+                          "Play", 24, (0, 0, 0), (0, 0, 0))
 
     running = True
     lmb_pressed = False
     rmb_pressed = False
+    paused = True
     mode = 1
 
     while running:
@@ -31,6 +35,12 @@ def run():
                     if reset_button.Rect.collidepoint(event.pos):
                         reset()
                         mode = 0  # Don't draw on button when clicked
+                    elif pause_button.Rect.collidepoint(event.pos):
+                        if paused:
+                            pause_button.change_text("Pause")
+                        else:
+                            pause_button.change_text("Play")
+                        paused = not paused
                 if event.button == 3:  # RMB
                     rmb_pressed = True
             elif event.type == pg.MOUSEBUTTONUP:
@@ -42,29 +52,32 @@ def run():
                 width, height = event.w, event.h
                 screen = pg.display.set_mode((width, height), pg.RESIZABLE)
                 reset_button.Rect.y = height - 50
+                pause_button.Rect.y = height - 50
 
         reset_button.draw()
+        pause_button.draw()
 
-        if lmb_pressed:  # Define behavior for lmb pressed down
-            grid_pos = get_grid_pos()
+        if not paused:
+            if lmb_pressed:  # Define behavior for lmb pressed down
+                grid_pos = get_grid_pos()
 
-            if not mode:  # If block exists at pressed position then remove it
-                try:
-                    wall_blocks.pop(grid_pos)
-                except KeyError:
-                    pass
-            else:
-                if not reset_button.Rect.collidepoint(grid_pos):
-                    wall_blocks.update({grid_pos: create_block(grid_pos, pixel_size)})  # else add it
+                if not mode:  # If block exists at pressed position then remove it
                     try:
-                        water_blocks.pop(grid_pos)  # Try to remove water if adding wall on top of it
+                        wall_blocks.pop(grid_pos)
                     except KeyError:
                         pass
+                else:
+                    if not reset_button.Rect.collidepoint(grid_pos) and not pause_button.Rect.collidepoint(grid_pos):
+                        wall_blocks.update({grid_pos: create_block(grid_pos, pixel_size)})  # else add it
+                        try:
+                            water_blocks.pop(grid_pos)  # Try to remove water if adding wall on top of it
+                        except KeyError:
+                            pass
 
-        if rmb_pressed:
-            grid_pos = get_grid_pos()
-            if grid_pos not in wall_blocks.keys():  # Don't put water on walls
-                water_blocks.update({grid_pos: create_block(grid_pos, pixel_size)})
+            if rmb_pressed:
+                grid_pos = get_grid_pos()
+                if grid_pos not in wall_blocks.keys():  # Don't put water on walls
+                    water_blocks.update({grid_pos: create_block(grid_pos, pixel_size)})
 
         for key in wall_blocks.keys():
             pg.Surface.fill(screen, wall_color, wall_blocks[key])  # Add every wall to surface
